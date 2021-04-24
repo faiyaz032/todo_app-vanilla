190bd083d74e738357e592c32df5cce9995a7474
Faiyaz Rahman <faiyazrahman03@gmail.com>
2021-04-24 16:16:10 +0600

    added filter system and used local storage

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector('.filter-todo');
 
 //* Event Listener
+window.addEventListener('load', getTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', completeOrDelete);
+filterOption.addEventListener('click', filterTodo);
 //* functions
 
 //! Add elements to the Todo Div
@@ -18,7 +20,7 @@ function addTodo(e) {
    todoDiv.classList.add('todo');
    const newTodo = createNewTodoListItem();
    todoDiv.appendChild(newTodo);
-
+   saveTodoToLocalStorage(todoInput.value);
    //check Mark Button
    const completedButton = createCheckMarkButton();
    todoDiv.appendChild(completedButton);
@@ -33,9 +35,9 @@ function addTodo(e) {
 }
 
 //!Create Elements of todo Div
-function createNewTodoListItem() {
+function createNewTodoListItem(todoValue = todoInput.value) {
    const newTodo = document.createElement('li');
-   newTodo.innerText = todoInput.value;
+   newTodo.innerText = todoValue;
    newTodo.classList.add('todo-item');
    return newTodo;
 }
@@ -69,4 +71,62 @@ function completeOrDelete(e) {
       const todo = item.parentElement;
       todo.classList.toggle('completed');
    }
+}
+
+function filterTodo(e) {
+   const todos = todoList.childNodes;
+   todos.forEach((todo) => {
+      switch (e.target.value) {
+         case 'all':
+            todo.style.display = 'flex';
+            break;
+
+         case 'completed':
+            if (todo.classList.contains('completed')) todo.style.display = 'flex';
+            else todo.style.display = 'none';
+            break;
+
+         case 'uncompleted':
+            if (!todo.classList.contains('completed')) todo.style.display = 'flex';
+            else todo.style.display = 'none';
+            break;
+      }
+   });
+}
+
+//! Working with Local Storage
+function saveTodoToLocalStorage(todo) {
+   let todos;
+   if (localStorage.getItem('todos') === null) {
+      todos = [];
+   } else {
+      todos = JSON.parse(localStorage.getItem('todos'));
+   }
+   todos.push(todo);
+   localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function getTodos() {
+   let todos;
+   if (localStorage.getItem('todos') === null) {
+      todos = [];
+   } else {
+      todos = JSON.parse(localStorage.getItem('todos'));
+   }
+   todos.forEach((todo) => {
+      const todoDiv = document.createElement('div');
+      todoDiv.classList.add('todo');
+      const newTodo = createNewTodoListItem(todo);
+      todoDiv.appendChild(newTodo);
+      //check Mark Button
+      const completedButton = createCheckMarkButton();
+      todoDiv.appendChild(completedButton);
+
+      //Trash Button
+      const trashButton = createTrashButton();
+      todoDiv.appendChild(trashButton);
+
+      //Append to ul
+      todoList.appendChild(todoDiv);
+   });
 }
